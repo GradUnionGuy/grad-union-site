e2e7815a64d88ea334173d18f601ad4ad3bad4da
GradUnionGuy <gradunionguy@gmail.com>
2025-02-14 20:40:07 -0600

    adding google sheets connectivity

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+from shillelagh.backends.apsw.db import connect
 import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYCAA')
+
+
+
+connection = connect(":memory:",
+                     adapter_kwargs = {
+                            "gsheetsapi": { 
+                            "service_account_info":  st.secrets["gcp_service_account"] 
+                                    }
+                                        }
+                        )
